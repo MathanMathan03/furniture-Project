7f200b9dbef523f1897bb16d9d41405501b4277e
MathanMathan03 <mathankumar936131@gmail.com>
2023-12-26 10:51:17 +0530

    change

--- src/app/Authentication/login/login.component.ts
+++ src/app/Authentication/login/login.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent  {
+  [x: string]: any;
     
   email:string='';
   password:string='';
+  log:any[]=[]
+  constructor(private api:ApiService){}
+
+
 
    login(){
-     
+      let getapi="email="+this.email+"&password="+this.password;
+      this.api.getUser(getapi).subscribe((reponse:any)=>{
+        this.log=reponse;
+        if(this.log.length>0){
+          alert('login sucessful')
+          sessionStorage.setItem("email",this.log[0].email)
+
+        }
+        
+      }) 
    }
 }
